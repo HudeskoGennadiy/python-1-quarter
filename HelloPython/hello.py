18bbdfd57dfd18a14a25d483bc3bd1cb5be7cf8c
Gennadiy Khudesko <gennadiy.18224@gmail.com>
2022-11-25 11:31:12 +0300

    created a new repository

---
+++ HelloPython/hello.py
@@ -0,0 +1,10 @@
+# типы данных и переменная
+# int, float, boolean, str, list, None
+
+
+a = 123
+b = 1.23
+
+print(a)
+print(b)
+
---
+++ Sem1/task1.py
@@ -0,0 +1,11 @@
+# вывести квадрат числа
+
+
+
+a = int(input())
+b = int(input())
+
+if a == b**2 or a**2 ==b:
+    print(f'Является')
+else:
+    print(f'НЕ Является')
